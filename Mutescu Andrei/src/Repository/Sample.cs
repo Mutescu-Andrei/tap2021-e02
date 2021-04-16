4a3210c412aa32161892e167f8859d88c154e5f3
Mutescu Andrei-Ioan <amutescu@yahoo.com>
2021-04-16 09:01:30 +0300

    (Unfinished Sql connection) Sql Datbase connect + Sample class made with the Guid/string/DataTime

---
+++ Mutescu Andrei/src/Repository/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+public class Sample
+{
+	public Sample(Guid Id,string DnaSequence,DateTime AnalysisTime)
+	{
+		Id = id;
+		DnaSequence = Dnasequence;
+		AnalysisTime = Analysistime;
+	}
+	public Guid Id { get; private set; }
+
+	public string DnaSequence { get; private set; }
+
+	public DateTime AnalysisTime { get; private set; }
+
+}
