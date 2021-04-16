28600905c6230ba8eee05aff7ded8c10c156e413
Mutescu Andrei-Ioan <amutescu@yahoo.com>
2021-04-16 09:39:39 +0300

    Extension for Dna Is vallid

---
+++ Mutescu Andrei/src/Repository/IsValidDnaSequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+	public static class DnaExtension()
+
+	{
+		public static bool(this string DnaSequence)
+		{ int count1 = 0, count2 = 0, count3 = 0, count4 = 0
+		if (DnaSequence.length = 1000)
+		{
+			foreach (char c in DnaSequence)
+			{ if (c == 'A') count1++;
+				if (c == 'C') count2++;
+				if (c == 'G') count3++;
+				if (c == 'T') count4++;
+			}
+
+		}
+		if (count1 + count2 + count3 + count4 != 1000)
+			return false;
+		return true;
+				
+}
+}
+}
